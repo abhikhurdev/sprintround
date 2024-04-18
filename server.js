8ed45a5a8a5874a8278a1e1d627ea9fc9ebc1cda
abhikhur_dev <abhimanyukhurana007@gmail.com>
2024-04-18 16:24:48 -0500

    more fixes balls 5

--- server.js
+++ server.js
@@ -10,21 +10,7 @@ const wss = new WebSocket.Server({ server });
 
 app.use(express.static('public'));
 
-const dataPath = path.join(__dirname, 'scoreboard.json');
-
 let teams = {};
-
-function loadTeams() {
-    if (fs.existsSync(dataPath)) {
-        teams = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
-    }
-}
-
-function saveTeams() {
-    fs.writeFileSync(dataPath, JSON.stringify(teams, null, 2), 'utf8');
-}
-
-loadTeams();
 
 const broadcast = (data) => {
     wss.clients.forEach(client => {
@@ -38,16 +24,12 @@ wss.on('connection', (ws) => {
   ws.send(JSON.stringify({ type: 'update', data: teams }));
   console.log('Client connected');
   ws.isAlive = true;
-
   ws.on('pong', () => {
-      ws.isAlive = true;
-  });
-
+    ws.isAlive = true;
+});
   
 
-  ws.on('close', () => {
-      console.log('WebSocket connection closed');
-  });
+ 
     ws.on('message', (message) => {
         const data = JSON.parse(message);
         switch (data.type) {
@@ -67,6 +49,9 @@ wss.on('connection', (ws) => {
         saveTeams();
         broadcast({ type: 'update', data: teams });
     });
+    ws.on('close', () => {
+      console.log('WebSocket connection closed');
+  });
 });
 
 // Ping every 30 seconds to keep the connections alive
@@ -77,7 +62,7 @@ setInterval(() => {
       ws.isAlive = false;
       ws.ping(null, false, true);
   });
-}, 30000);
+}, 5000);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
